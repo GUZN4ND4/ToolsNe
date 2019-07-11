16b406a413e74cb880ad98e714feabb24ee1e74c
GUZN4ND4 <52776848+GUZN4ND4@users.noreply.github.com>
2019-07-11 15:49:32 +0800

    Add files via upload

---
+++ ToolsNe.sh
@@ -0,0 +1,174 @@
+#!/bin/sh
+
+Cy='\033[34;1m' #biru            
+i='\033[32;1m' #ijo    
+pur='\033[35;1m' #purple
+e='\033[36;1m' #cyan 
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+clear
+figlet Tools Ne Jall!!
+figlet follow ig guznanda_
+sleep 2
+echo $me "─────▄████▀█▄ 
+───▄█████████████████▄
+─▄█████.▼.▼.▼.▼.▼.▼▼▼▼
+▄███████▄.▲.▲▲▲▲▲▲▲▲ 
+████████████████████▀▀" | lolcat
+
+echo " Loading_Sayang" | lolcat
+
+
+sleep 4
+
+
+echo "[ 1 ] Install Bahan
+[ 2 ] Spam Call
+[ 3 ] Spam Sms
+[ 4 ] Lacak Lokasi Via Web Phising
+[ 5 ] DDOS HAMMER
+[ 6 ] Buat Sc Deface 
+[ 7 ] Tools Hack FB
+[ 9 ]  Hack FB [ user: Anak  pass:Bangsa ]
+[ 10 ] 10 DDOS  ATTACK
+[ 87 ] Exit " | lolcat
+
+echo "==============================[GuzN4ND4]==============================
+ Pilih nomor => " | lolcat
+ read nomor
+ 
+
+if [ $nomor = 1 ] || [ $nomor 01 ]
+then
+clear
+figlet Sabar ya.. | lolcat
+pkg install unzip
+pkg install php
+pkg install nodejs
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip install requests
+pip2 install requests
+pip install mechanize
+pkg install unstable-repo
+pip2 install mechanize
+pkg install curl
+pkg install openssh
+pkg install php
+pjg install python
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " S U K S E S "|lolcat
+fi
+
+if [ $nomor = 2 ] || [ $nomor = 02 ]
+then
+clear
+pkg update 
+pkg upgrade 
+pkg install git
+git clone https://github.com/T14RB373T/FAKECALL
+cd FAKECALL
+bash call.sh
+fi
+
+if [ $nomor = 3 ] || [ $nomor = 03 ]
+then
+clear
+figlet SPAM SMS | lolcat
+pkg update 
+pkg upgrade
+pkg install git
+git clone https://github.com/KANG-NEWBIE/SpamSms
+cd SpamSms
+python main.py
+fi
+
+if [ $nomor = 4 ] || [ $nomor = 04 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/locator
+cd locator
+bash locator.sh
+fi
+
+if [ $nomor 5 ] || [ $nomor 05]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/cyweb/hammer
+cd hammer
+python3 hammer.py
+fi
+
+if [ $nomor = 6 ] || [ $nomor 06 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/404rgr/webdav77
+cd webdav77
+chmod +x *
+./install.sh
+bash webdav77.sh
+fi
+
+if [ $nomor = 7 ] || [ $nomor = 07 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+sh new.sh
+fi
+
+if [ $nomor = 9 ] || [ $nomor = 09 ]
+then
+clear
+figlet Sabar ya..... | lolcat
+apt update && apt upgrade
+pkg ins toilet
+pkg ins figlet
+pkg ins ruby
+pkg ins gem
+gem ins lolcat
+pkg ins php
+pkg ins python2
+pkg ins git
+git clone https://github.com/Rusmana-ID/rus
+cd rus
+sh v2.sh
+fi
+
+if [ $nomor =10 ] || [ $nomor = 10 ] 
+then
+clear
+figlet -f slant "W A I T"|lolcat
+git clone https://github.com/R133F/10Attacker
+cd 10Attacker
+sh attack.sh
+fi
+
+if [ $nomor = 87 ] || [ $nomor = 87 ]
+then
+clear
+figlet E X I T | lolcat
+sleep 2
+clear
+figlet IG guznanda_ | lolcat
+sleep 2
+exit
+fi
